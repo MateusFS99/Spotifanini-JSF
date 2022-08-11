93c7145224410e2f16a6af68ce77a45ba5d84cf6
MateusFS99 <mateusfsantos.pfs@gmail.com>
2022-08-11 14:35:21 -0300

    - Little Changes in Layout.

--- build/web/resources/js/script.js
+++ build/web/resources/js/script.js
@@ -1,8 +1,9 @@
 var audioPlayer = document.getElementById('audio-player');
 var btnPlay = document.getElementById('btnPlay');
 var btnPause = document.getElementById('btnPause');
-var progressBar = document.querySelector('.player-control-progress-2');
-var loaded = false;
+var progressBar = document.querySelector('.player-control-track-progress-2');
+var currentTime = document.getElementById('current-time');
+var fullTime = document.getElementById('full-time');
 var interval = null;
 
 btnPause.addEventListener('click', (e) => {
@@ -12,37 +13,48 @@ btnPause.addEventListener('click', (e) => {
     audioPlayer.pause();
     btnPlay.style.display = "inline";
     btnPause.style.display = "none";
-
-    return false;
 });
 
 btnPlay.addEventListener('click', (e) => {
 
-    e.preventDefault();
-    play();
-
-    return false;
+    if(document.getElementById('musica-rodando') !== null) {
+        
+        e.preventDefault();
+        play();
+    }
 });
+
+let getTime = (milli) => {
+    
+    let time = new Date(milli);
+    let minutes = time.getUTCMinutes();
+    let seconds = time.getUTCSeconds();
+    
+    if(seconds < 10)
+        seconds = "0" + seconds;
+    return minutes + ":" + seconds;
+}
 
 function play() {
 
     interval = setInterval(() => {
         
         progressBar.style.width = (audioPlayer.currentTime / audioPlayer.duration * 100) + "%";
+        currentTime.innerHTML = `<p>`+ getTime(audioPlayer.currentTime * 1000) +`</p>`;
+        fullTime.innerHTML = `<p>`+ getTime(audioPlayer.duration * 1000) +`</p>`;
     }, 1000);
-    audioPlayer.volume = .1;
+    audioPlayer.volume = .05;
     audioPlayer.play();
+    fullTime.style.display = "inline-block";
+    currentTime.style.display = "inline-block";
     btnPlay.style.display = "none";
     btnPause.style.display = "inline";
 }
 
 const playSong = (file) => {
 
-    if(loaded == false) {
-
-        audioPlayer.innerHTML = `<source src="` + file + `" type="audio/mp3">`;
-        loaded = true;
-    }
+    audioPlayer.innerHTML = `<source id="source-player" src="` + file + `" type="audio/mp3">`;
+    audioPlayer.load();
     play();
 }
 
@@ -58,7 +70,7 @@ document.querySelectorAll('#main-col-music').forEach(item => {
 
         playerArtistComponent[0].innerHTML = `
 
-            <img src="` + image + `"/>
+            <img id="musica-rodando" src="` + image + `"/>
             <h3>` + song + `<br/><span>` + artist + `<span><h3>
         `;
 
